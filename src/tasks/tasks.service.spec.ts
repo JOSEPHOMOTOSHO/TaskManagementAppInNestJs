6d723063760ca8ce5703a4d84833a82060837508
Omotosho Joseph <Joseph.omotosho98@gmail.com>
2021-12-07 08:30:07 +0100

    added some more tests

--- src/tasks/tasks.service.spec.ts
+++ src/tasks/tasks.service.spec.ts
@@ -1,9 +1,13 @@
 import { Test } from '@nestjs/testing';
 import { TasksService } from './tasks.service';
 import { TaskRepository } from './task.Repository';
+import { title } from 'process';
+import { TaskStatus } from './task-status.enum';
+import { NotFoundException } from '@nestjs/common';
 
 const mockTaskRepository = () => ({
   getTasks: jest.fn(),
+  findOne: jest.fn(),
 });
 
 const user = {
@@ -31,9 +35,30 @@ describe('Testing the Tasks Service', () => {
 
   describe('getTasks', () => {
     it('calls the taskRepository.getTasks and it should return a result', async () => {
-      taskRepository.getTasks.mockResolvedValue('resolved-Value');
-      const result = await taskRepository.getTasks(null, user);
-      expect(result).toEqual('resolved-Value');
+      taskRepository.getTasks.mockResolvedValue('someValue');
+      const result = await taskService.getTasks(null, user);
+      expect(result).toEqual('someValue');
+    });
+  });
+
+  describe('getTask by Id', () => {
+    it('calls the taskRepository.findOne and it should return a result', async () => {
+      let mockTask = {
+        id: '1',
+        title: 'we doing great',
+        description: 'yea we are',
+        status: TaskStatus.OPEN,
+      };
+      taskRepository.findOne.mockResolvedValue(mockTask);
+      const result = await taskService.getSingleTask('someId', user);
+      expect(result).toEqual(mockTask);
+    });
+    it('calls the taskRepository.findOne  and it should throw an error', async () => {
+      taskRepository.findOne.mockResolvedValue(null);
+
+      expect(taskService.getSingleTask('someid', user)).rejects.toThrow(
+        NotFoundException,
+      );
     });
   });
 });
